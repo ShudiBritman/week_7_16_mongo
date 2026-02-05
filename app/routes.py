27f325e539940c4d09fb31b469042f8d798b4ad5
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-02-05 11:33:43 +0200

    fix routes

--- app/routes.py
+++ app/routes.py
@@ -3,33 +3,33 @@ from dal import *
 
 
 
-router = APIRouter('/employees')
+router = APIRouter()
 
 
-router.get("/engineering/high-salary")
+router.get("/employees/engineering/high-salary")
 async def engineering_high_salary_employees():
     return engineering_high_salary_employees()
 
 
-router.get("/by-age-and-role")
+router.get("/employees/by-age-and-role")
 async def employees_by_age_and_role():
     return employees_by_age_and_role()
 
 
-router.get("/top-seniority")
+router.get("/employees/top-seniority")
 async def top_seniority_employees_excluding_hr():
     return get_top_seniority_employees_excluding_hr()
 
 
-router.get("/age-or-seniority")
+router.get("/employees/age-or-seniority")
 async def mployees_by_age_or_seniority():
     return get_employees_by_age_or_seniority()
 
 
-router.get("/managers/excluding-departments")
+router.get("/employees/managers/excluding-departments")
 async def managers_excluding_departments():
     return get_managers_excluding_departments()
 
-router.get("/by-lastname-and-age")
+router.get("/employees/by-lastname-and-age")
 async def employees_by_lastname_and_age():
     return get_employees_by_lastname_and_age()
