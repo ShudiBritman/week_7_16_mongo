2cccf0bbae8242f3303da9f03193905c12d9c040
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-02-05 12:59:47 +0200

    fix server

--- app/main.py
+++ app/main.py
@@ -18,31 +18,31 @@ async def health():
 
 @app.get("/employees/engineering/high-salary")
 async def engineering_high_salary_employees():
-    return await get_engineering_high_salary_employees()
+    return  get_engineering_high_salary_employees()
 
 
 @app.get("/employees/by-age-and-role")
 async def employees_by_age_and_role():
-    return await get_employees_by_age_and_role()
+    return get_employees_by_age_and_role()
 
 
 @app.get("/employees/top-seniority")
 async def top_seniority_employees_excluding_hr():
-    return await get_top_seniority_employees_excluding_hr()
+    return get_top_seniority_employees_excluding_hr()
 
 
 @app.get("/employees/age-or-seniority")
 async def mployees_by_age_or_seniority():
-    return await get_employees_by_age_or_seniority()
+    return get_employees_by_age_or_seniority()
 
 
 @app.get("/employees/managers/excluding-departments")
 async def managers_excluding_departments():
-    return await get_managers_excluding_departments()
+    return get_managers_excluding_departments()
 
 @app.get("/employees/by-lastname-and-age")
 async def employees_by_lastname_and_age():
-    return await get_employees_by_lastname_and_age()
+    return get_employees_by_lastname_and_age()
 
 
 if __name__ == "__main__":
