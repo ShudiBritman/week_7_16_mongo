b462ee240f39dc68ccb30288a1141345e249952d
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-02-05 11:53:36 +0200

    end server

--- app/main.py
+++ app/main.py
@@ -1,22 +1,48 @@
 from fastapi import FastAPI
-from contextlib import asynccontextmanager
+from dal import *
 import uvicorn
-from routes import router
 from db import init_db
 
-@asynccontextmanager
-async def lifespan(app: FastAPI):
+
+app = FastAPI()
+    
+@app.on_event("startup")
+async def lifespan():
     init_db()
 
-app = FastAPI(lifespan=lifespan)
-    
-app.include_router(
-    router
-)
+
 
 @app.get("/health")
-def health():
+async def health():
     return {"status": "ok"}
+
+@app.get("/employees/engineering/high-salary")
+async def engineering_high_salary_employees():
+    return engineering_high_salary_employees()
+
+
+@app.get("/employees/by-age-and-role")
+async def employees_by_age_and_role():
+    return employees_by_age_and_role()
+
+
+@app.get("/employees/top-seniority")
+async def top_seniority_employees_excluding_hr():
+    return get_top_seniority_employees_excluding_hr()
+
+
+@app.get("/employees/age-or-seniority")
+async def mployees_by_age_or_seniority():
+    return get_employees_by_age_or_seniority()
+
+
+@app.get("/employees/managers/excluding-departments")
+async def managers_excluding_departments():
+    return get_managers_excluding_departments()
+
+@app.get("/employees/by-lastname-and-age")
+async def employees_by_lastname_and_age():
+    return get_employees_by_lastname_and_age()
 
 
 if __name__ == "__main__":
