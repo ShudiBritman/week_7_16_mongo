42f14aff495d65a49b82657f12ca7ddacfb82f47
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-02-05 09:37:25 +0200

    add init db func

--- app/db.py
+++ app/db.py
@@ -1,10 +1,26 @@
 from connection import MongoConnection
 import os
+import json
 
 def get_db():
-    db_name = os.getenv("MONGO_DB", "my_db")
+    db_name = os.getenv("MONGO_DB", "employees")
     client = MongoConnection().get_client()
     return client[db_name]
 
 def get_collection(collection_name: str):
     return get_db()[collection_name]
+
+
+def init_db():
+
+    col = get_collection('employees')
+    if col.count_documents({"init": True}) == 0:
+        with open("app/init_data.json") as f:
+            data = json.load(f)
+
+        col.insert_many(data)
+        col.insert_one({"init": True})
+
+        print("DB initialized")
+    else:
+        print("DB already initialized")
