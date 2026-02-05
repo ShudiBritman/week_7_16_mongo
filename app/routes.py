80d268e9c2e2abaa88e868ca2d2b9e05f6c72ca2
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-02-05 11:23:46 +0200

    add async to endpoints

--- app/routes.py
+++ app/routes.py
@@ -7,29 +7,29 @@ router = APIRouter('/employees')
 
 
 router.get("/engineering/high-salary")
-def engineering_high_salary_employees():
+async def engineering_high_salary_employees():
     return engineering_high_salary_employees()
 
 
 router.get("/by-age-and-role")
-def employees_by_age_and_role():
+async def employees_by_age_and_role():
     return employees_by_age_and_role()
 
 
 router.get("/top-seniority")
-def top_seniority_employees_excluding_hr():
+async def top_seniority_employees_excluding_hr():
     return get_top_seniority_employees_excluding_hr()
 
 
 router.get("/age-or-seniority")
-def mployees_by_age_or_seniority():
+async def mployees_by_age_or_seniority():
     return get_employees_by_age_or_seniority()
 
 
 router.get("/managers/excluding-departments")
-def managers_excluding_departments():
+async def managers_excluding_departments():
     return get_managers_excluding_departments()
 
 router.get("/by-lastname-and-age")
-def employees_by_lastname_and_age():
+async def employees_by_lastname_and_age():
     return get_employees_by_lastname_and_age()
