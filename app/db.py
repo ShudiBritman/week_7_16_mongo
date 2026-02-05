b9a3e03641d2fb3ea56a782c4846823ab337f9a6
Shudi <sdbritman@gmail.comgit config --listgit config --listgit config --global user.name>
2026-02-05 09:43:51 +0200

    fix init db

--- app/db.py
+++ app/db.py
@@ -3,13 +3,12 @@ import os
 import json
 
 
-
+PATH = '../data'
 
 def init_db():
-
-    col = get_collection('employees')
+    col = MongoConnection().get_collection('employees')
     if col.count_documents({"init": True}) == 0:
-        with open("app/init_data.json") as f:
+        with open(PATH) as f:
             data = json.load(f)
 
         col.insert_many(data)
